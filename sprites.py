38d0de889d100fefdceb509f9ebd100f128d86dd
Tom Parish <1tomparish@gmail.com>
2021-08-12 22:08:35 +1000

    mostly art additions

--- sprites.py
+++ sprites.py
@@ -4,7 +4,7 @@ class player():
     def __init__(self, x, y):
         self.width = 20
         self.height = 40
-        self.image = pg.transform.scale(pg.image.load('assets/images/player.png'), (self.width,self.height))
+        self.image = pg.transform.scale(pg.image.load('assets/images/mainchar.png'), (self.width,self.height))
         self.x = x
         self.y = y
         self.vel = 3
